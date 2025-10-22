eb65c344be6a2e9dba2c33e7bf1488823bdc51e5
simuneu <worksimu@gmail.com>
2025-10-22 17:29:56 +0900

    feat:이메일 인증 엔티티 추가

---
+++ src/main/java/com/budgie/server/entity/EmailVerificationEntity.java
@@ -0,0 +1,42 @@
+package com.budgie.server.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.userdetails.User;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="EmailVerification")
+public class EmailVerificationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="email_id", nullable = false)
+    private long emailId;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private UserEntity userId;
+
+    @Column(name="email", nullable = false, length = 255, unique = true)
+    private String email;
+
+    @Column(name="valid_code", nullable = false, length = 50)
+    private String validCode;
+
+    @Column(name="expiration_time")
+    private Instant expirationTime;
+
+    @CreationTimestamp
+    @Column(name="created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
